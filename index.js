f3041b39981b59266b8f530339b9510c4d4a9e43
Alina L <alodahl@pointloma.edu>
2017-09-22 14:34:54 -0700

    added mvp html, function stubs in js, and centering in css

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function getDataFromApi() {
+  //make 1 JSON query for 1 free API
+  //call AJAX via jQuery
+  //return result data to displaySearchData()
+}
+
+function renderResult() {
+  //use string interpolation to create a div containing API recipe data
+  //data needed: small photo, title, author
+}
+
+function displaySearchData(data) {
+  //use jQuery to append "We have # results for you:"
+  //use jQuery to renderResult(data) and append the first 12 results to the DOM
+  //use jQuery to remove 'hidden' aria attr from main
+
+}
+
+function watchForClicks() {
+//create event listener for add-ingredient button
+    //append input term to the dom: top of main
+    //send input term to query
+
+//create event listener for result clicks,
+    //which should open a linked recipe page
+
+}
+
+
+$(watchForClicks);
