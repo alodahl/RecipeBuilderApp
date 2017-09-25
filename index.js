7c3fd3de6ce016b9cff772e1153654d537594e9e
Alina L <alodahl@pointloma.edu>
2017-09-25 14:56:25 -0700

    fixed ingredient remove buttons, some form styling

--- index.js
+++ index.js
@@ -4,49 +4,48 @@ let queryArray = [];
 
 function getDataFromApi(searchTerm , callback) {
   const settings = {
-  url: 'https://api.edamam.com/search',
-  data: {
-    q: `${searchTerm} in:ingredients`,
-    app_id: '37073675',
-    app_key: '46b633f590a05be11cab8a438977deb9',
-    to: 8,
-    part: 'hits'
-  },
-  dataType: 'json',
-  type: 'GET',
-  success: callback
-};
-$.ajax(settings);
+    url: 'https://api.edamam.com/search',
+    data: {
+      q: `${searchTerm} in:ingredients`,
+      app_id: '37073675',
+      app_key: '46b633f590a05be11cab8a438977deb9',
+      to: 8,
+      part: 'hits'
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
 }
 
 function renderResult(item, index) {
   let recipeResult = `<span class="thumbnail">
-                        <a href="${item.recipe.url}" aria-label="${item.recipe.label}"  target="_blank"><img src="${item.recipe.image}">
-                        <h3>${item.recipe.label}</h3></a>
-                        <h4>by ${item.recipe.source}</h4>
-                        <p>${item.recipe.ingredients.length} ingredients</p></span>`;
+  <a href="${item.recipe.url}" aria-label="${item.recipe.label}"  target="_blank"><img src="${item.recipe.image}">
+  <h3>${item.recipe.label}</h3></a>
+  <h4>by ${item.recipe.source}</h4>
+  <p>${item.recipe.ingredients.length} ingredients</p></span>`;
   return recipeResult;
 }
 
 function displaySearchData(data) {
   console.log('displaySearchData is running');
   console.log(data);
-  console.log(data.count);
   const results = data.hits.map((item, index) => renderResult(item, index));
 
-  $('.js-resultNum').replaceWith(data.count);
+  $('.js-resultNum').text(data.count);
   $('h2').prop("hidden", false);
   $('.js-results').html(results);
 }
 
 function renderIngrButton(item, index) {
-  let button = `<button class="remove-added" type="button" id="${index}">${item} X</button>`;
+  let button = `<button class="added-ingredient" type="button" data-index="${index}">${item} X</button>`;
   return button;
 }
 
 function displayAddedIngredients() {
   const ingredients = queryArray.map((item, index) => renderIngrButton(item, index));
-  $('.js-added').html(ingredients);
+  $('.js-added-ingredient-list').html(ingredients);
 }
 
 
@@ -67,8 +66,10 @@ function watchForClicks() {
     getDataFromApi(queryArray, displaySearchData);
   })
 
-  $('.js-added').on('click', function(event) {
-    
+  $('.js-added-ingredient-list').on('click', '.added-ingredient', function(event) {
+    console.log("ingredient button clicked");
+    var index = $(this).attr('data-index');
+    queryArray.splice(index, 1);
     displayAddedIngredients();
   })
 }
