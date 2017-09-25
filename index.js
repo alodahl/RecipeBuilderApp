453f4f20c9a55c5ddc184935f1af8e8ba9ad9113
Alina L <alodahl@pointloma.edu>
2017-09-25 10:33:23 -0700

    linked recipe image and title, added minor styling, recipe results number displays in DOM

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function getDataFromApi(searchTerm , callback) {
     q: `${searchTerm} in:ingredients`,
     app_id: '37073675',
     app_key: '46b633f590a05be11cab8a438977deb9',
-//46b633f590a05be11cab8a438977deb9	—
     to: 8,
     part: 'hits'
   },
@@ -15,31 +14,32 @@ function getDataFromApi(searchTerm , callback) {
   type: 'GET',
   success: callback
 };
-$.ajax(settings);//make 1 JSON query for 1 free API
-  //return result data to displaySearchData()
+$.ajax(settings);
 }
 
 function renderResult(item, index) {
   let recipeResult = `<span class="thumbnail">
-                        <img src="${item.recipe.image}">
-                        <h3>${item.recipe.label}</h3>
+                        <a href="${item.recipe.url}" aria-label="${item.recipe.label}"  target="_blank"><img src="${item.recipe.image}">
+                        <h3>${item.recipe.label}</h3></a>
                         <h4>by ${item.recipe.source}</h4>
                         <p>${item.recipe.ingredients.length} ingredients</p></span>`;
-  //use string interpolation to create a div containing API recipe data
-  //data needed: small photo, title, author
   return recipeResult;
 }
 
 function displaySearchData(data) {
   console.log('displaySearchData is running');
   console.log(data);
+  console.log(data.count);
   const results = data.hits.map((item, index) => renderResult(item, index));
-  $('.results').html(results);
+
+  $('.js-resultNum').replaceWith(data.count);
+  $('h2').prop("hidden", false);
+  $('.js-results').html(results);
 }
 
 function watchForClicks() {
 //create event listener for add-ingredient button
-$('.ingr-search-form').submit(event => {
+$('.js-ingr-search-form').submit(event => {
     console.log("add-ingredient button is working");
     event.preventDefault();
     const queryTarget = $(event.currentTarget).find('.js-query');
