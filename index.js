3fa8718b819314ab70ee3624ab92cdfde41e78fa
Alina L <alodahl@pointloma.edu>
2017-09-25 16:49:18 -0700

    added empty responsive modal

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ function getDataFromApi(searchTerm , callback) {
   $.ajax(settings);
 }
 
+// <a href="${item.recipe.url}" aria-label="${item.recipe.label}"  target="_blank"></a>
 function renderResult(item, index) {
   let recipeResult = `<span class="thumbnail">
-  <a href="${item.recipe.url}" aria-label="${item.recipe.label}"  target="_blank"><img src="${item.recipe.image}">
-  <h3>${item.recipe.label}</h3></a>
+  <img src="${item.recipe.image}" alt="${item.recipe.label}">
+  <h3>${item.recipe.label}</h3>
   <h4>by ${item.recipe.source}</h4>
   <p>${item.recipe.ingredients.length} ingredients</p></span>`;
   return recipeResult;
@@ -71,6 +72,24 @@ function watchForClicks() {
     var index = $(this).attr('data-index');
     queryArray.splice(index, 1);
     displayAddedIngredients();
+  })
+
+  $('.js-results').on('click', ".thumbnail", function(event) {
+    // var videoId = $(this).attr("data-id"); //use the id of the selected video
+    // var video  = "https://www.youtube.com/embed/"+videoId; //link to embed the chosen video
+    // $('#video').attr("src", video);  //place link in the video div
+    $('.modal').removeClass("hidden");  //then show the div
+    // $('.js-search-results').prop("hidden", true);
+    // $('h1').prop("hidden", true);
+    // $('form').prop("hidden", true);
+  })
+
+  $('.close-button').on('click', function(event) {
+    $('.modal').addClass("hidden");
+    // $('#video').attr("src", "");
+    // $('.js-search-results').prop("hidden", false);
+    // $('h1').prop("hidden", false);
+    // $('form').prop("hidden", false);
   })
 }
 
